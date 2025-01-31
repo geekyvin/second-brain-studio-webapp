b70f348e3dc9d255befd97fe0f5dae1a5029b440
Vinoth Kumar Asaithambi <vinoth@192.168.1.114>
2025-01-31 20:27:25 +0000

    basic-markdown-editor-working

--- src/second_brain_studio_webapp/prosemirror.cljs
+++ src/second_brain_studio_webapp/prosemirror.cljs
@@ -4,27 +4,39 @@
    ["prosemirror-model" :refer [Schema DOMParser DOMSerializer]]
    ["prosemirror-state" :refer [EditorState]]
    ["prosemirror-view" :refer [EditorView]]
-   ["prosemirror-schema-basic" :refer [schema]]))
+   ["prosemirror-schema-basic" :refer [schema]]
+   ["prosemirror-markdown" :refer [MarkdownParser defaultMarkdownParser markdownSyntax]]))
+
+;; Initialize markdown-parser properly
+;; (def markdown-parser (MarkdownParser. schema markdownSyntax))
+
+;; Use the default markdown parser (pre-configured)
+(def markdown-parser defaultMarkdownParser)
 
 (defn create-editor [element]
-  (let [editor-state (.create EditorState #js {:schema schema})]
+  ;; Parse initial markdown content
+  (let [doc (.parse markdown-parser "# Initial Markdown Content")
+        editor-state (.create EditorState #js {:doc doc
+                                               :schema schema})]
+    ;; Create and return the ProseMirror editor view
     (EditorView. element #js {:state editor-state})))
 
 (defn prosemirror-editor []
-  (let [editor-ref (r/atom nil)   ;; Reference to store the ProseMirror instance
-        dom-node (r/atom nil)]   ;; Reference to the DOM node for the editor
+  (let [editor-ref (r/atom nil)
+        dom-node (r/atom nil)]
     (r/create-class
      {:component-did-mount
       (fn [_]
         (when-let [node @dom-node]
-          (reset! editor-ref (create-editor node)))) ;; Initialize the editor
+          (reset! editor-ref (create-editor node))))
       :component-will-unmount
       (fn [_]
         (when-let [editor @editor-ref]
-          (.destroy editor))) ;; Clean up the editor instance
+          (.destroy editor)))
       :reagent-render
       (fn []
-        [:div#editor
-         {:ref #(reset! dom-node %)
-          :style {:border "1px solid #ccc"
-                  :min-height "300px"}}])})))
+        [:div
+         [:div#editor {:ref #(reset! dom-node %)
+                       :style {:border "1px solid #ccc"
+                               :min-height "300px"}}]])})))
+
--- src/second_brain_studio_webapp/views.cljs
+++ src/second_brain_studio_webapp/views.cljs
@@ -5,10 +5,11 @@
     [second-brain-studio-webapp.prosemirror :refer [prosemirror-editor]]))
   
 (defn main-panel []
+  (println "main-panel called")
+  (println "prosemirror-editor: " prosemirror-editor)
   (let [name (re-frame/subscribe [::subs/name])]
     [:div
-     [:h1
-      "Hello from " @name]
+     [:h1 "Hello from " @name]
      [prosemirror-editor]
-     ]))
+    ]))
 
