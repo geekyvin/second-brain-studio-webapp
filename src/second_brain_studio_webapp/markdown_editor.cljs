c4ea67a93c8f8335cfe4852f405b456102c32db7
Vinoth Kumar Asaithambi <vinoth@192.168.1.114>
2025-02-01 18:40:39 +0000

    This-works-lets-make-the-mvp

--- src/second_brain_studio_webapp/markdown_editor.cljs
+++ src/second_brain_studio_webapp/markdown_editor.cljs
@@ -8,28 +8,42 @@
 
 (defn markdown-editor []
   (let [content (r/atom "# Hello Markdown\n\nWrite some **Markdown** here!")
-        preview-html (r/atom nil)]
+        mode (r/atom :edit)] ;; Mode can be :edit or :preview
     (fn []
       [:div {:style {:display "flex"
-                     :gap "20px"
-                     :align-items "flex-start"}}
-       ;; Textarea for Markdown input
-       [:textarea {:value @content
-                   :on-change #(reset! content (-> % .-target .-value))
-                   :style {:width "50%"
-                           :height "400px"
-                           :padding "10px"
-                           :font-family "monospace"
-                           :font-size "14px"
-                           :border "1px solid #ccc"
-                           :border-radius "5px"}}]
-       ;; Markdown preview (HTML rendered)
-       [:div {:style {:width "50%"
-                      :height "400px"
-                      :overflow-y "auto"
-                      :padding "10px"
-                      :border "1px solid #ccc"
-                      :border-radius "5px"
-                      :background-color "#f9f9f9"}}
-        [:div {:dangerouslySetInnerHTML
-               #js {:__html (.render markdown-parser @content)}}]]])))
+                     :flex-direction "column"
+                     :gap "20px"}}
+       ;; Radio buttons to toggle modes
+       [:div {:style {:margin-bottom "10px"}}
+        [:label {:style {:margin-right "10px"}}
+         [:input {:type "radio"
+                  :name "mode"
+                  :checked (= @mode :edit)
+                  :on-change #(reset! mode :edit)}]
+         "Edit"]
+        [:label
+         [:input {:type "radio"
+                  :name "mode"
+                  :checked (= @mode :preview)
+                  :on-change #(reset! mode :preview)}]
+         "Preview"]]
+       ;; Conditionally render Edit or Preview mode
+       (case @mode
+         :edit [:textarea {:value @content
+                           :on-change #(reset! content (-> % .-target .-value))
+                           :style {:width "100%"
+                                   :height "400px"
+                                   :padding "10px"
+                                   :font-family "monospace"
+                                   :font-size "14px"
+                                   :border "1px solid #ccc"
+                                   :border-radius "5px"}}]
+         :preview [:div {:style {:width "100%"
+                                 :height "400px"
+                                 :overflow-y "auto"
+                                 :padding "10px"
+                                 :border "1px solid #ccc"
+                                 :border-radius "5px"
+                                 :background-color "#f9f9f9"}}
+                   [:div {:dangerouslySetInnerHTML
+                          #js {:__html (.render markdown-parser @content)}}]])])))
