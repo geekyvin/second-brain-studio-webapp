ae882dccbcfc5edd1f64c180df0410dcbe66db64
Vinoth Kumar Asaithambi <vinoth@192.168.1.114>
2025-02-05 15:57:20 +0000

    working-summarize-fumction

--- src/second_brain_studio_webapp/markdown_editor.cljs
+++ src/second_brain_studio_webapp/markdown_editor.cljs
@@ -2,16 +2,45 @@
   (:require
    [reagent.core :as r]
    ["markdown-it" :as MarkdownIt]
+   [clojure.string :as str]
    [second-brain-studio-webapp.note-view :as note-view]))
 
 ;; Initialize the Markdown parser
 (def markdown-parser (MarkdownIt.))
 
+(defn call-summarize-api [content on-success]
+  (-> (js/fetch "http://localhost:3000/get-summary"
+                #js {:method "POST"
+                     :headers #js {"Content-Type" "application/json"}
+                     :body (js/JSON.stringify #js {:content content})})
+      (.then (fn [response]
+               (if (.-ok response)
+                 (.json response)
+                 (throw (js/Error (str "HTTP error! status: " (.-status response)))))))
+      (.then (fn [data]
+               (js/console.log "API Response:" data)
+               (on-success (.-message data))))
+      (.catch (fn [error]
+                (js/console.error "Error fetching summary:" error)))))
+
+(defn update-summary-section [content summary]
+  ;; Find or create the `#### Summary` section
+  (let [lines (clojure.string/split content #"\n")
+        summary-index (.indexOf lines "#### Summary")
+        updated-lines (if (>= summary-index 0)
+                        ;; Replace existing summary section
+                        (concat (take (inc summary-index) lines) [summary])
+                        ;; Append new summary section
+                        (concat lines ["#### Summary" summary]))]
+    ;; Join the updated lines back into Markdown
+    (clojure.string/join "\n" updated-lines)))
+
 (defn markdown-editor []
   (let [content (r/atom "### Hello World!\n\nWrite some **Markdown** here!")
         mode (r/atom :edit)
         title (r/atom "Untitled") ;; Default title
-        editing-title (r/atom false)] ;; Track if the title is being edited
+        editing-title (r/atom false) ;; Track if the title is being edited
+        highlighted (r/atom false)] ;; Track if the summary is highlighted
     (fn []
       [:div {:style {:display "flex"
                      :height "100vh"
@@ -51,25 +80,28 @@
                          :cursor "pointer"}}
             @title])]
 
-        ;; Radio buttons to toggle modes
-        [:div {:style {:margin-bottom "10px"}}
-         [:label {:style {:margin-right "10px"}}
-          [:input {:type "radio"
-                   :name "mode"
-                   :checked (= @mode :edit)
-                   :on-change #(reset! mode :edit)}]
-          "Edit"]
-         [:label
-          [:input {:type "radio"
-                   :name "mode"
-                   :checked (= @mode :preview)
-                   :on-change #(reset! mode :preview)}]
-          "Preview"]]
+        ;; Summarize Button
+        [:button {:style {:margin-bottom "10px"
+                          :padding "10px"
+                          :background-color "#007BFF"
+                          :color "#fff"
+                          :border "none"
+                          :border-radius "5px"
+                          :cursor "pointer"}
+                  :on-click #(call-summarize-api @content
+                                                 (fn [summary]
+                                                   ;; Replace the Summary section
+                                                   (reset! content (update-summary-section @content summary))
+                                                   ;; Trigger highlight effect
+                                                   (reset! highlighted true)
+                                                   (js/setTimeout (fn [] (reset! highlighted false)) 2000)))}
+         "Summarize"]
 
         ;; Conditionally render Edit or Preview mode
         (case @mode
           :edit [:textarea {:value @content
                             :on-change #(reset! content (-> % .-target .-value))
+                            :class (when @highlighted "highlight") ;; Apply highlight class
                             :style {:width "100%"
                                     :height "70vh"
                                     :padding "10px"
@@ -86,4 +118,3 @@
                                   :background-color "#f9f9f9"}}
                     [:div {:dangerouslySetInnerHTML
                            #js {:__html (.render markdown-parser @content)}}]])]])))
-
